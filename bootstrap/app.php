37b70dfa377b45c87497be325b4e8cc446621b75
Alexander <admin@alxserver.de>
2026-09-02 22:32:28 +0200

    Enforce active users on authenticated requests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\RegistrationEnabled;
+use App\Http\Middleware\EnsureUserIsActive;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
@@ -18,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         // Admin-Middleware
         $middleware->alias([
             'admin' => AdminMiddleware::class,
+            'active' => EnsureUserIsActive::class,
         ]);
 
         $middleware->web(append: [
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/', function () {
 |--------------------------------------------------------------------------
 */
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'active'])->group(function () {
 
     /*
      * =========================================================
