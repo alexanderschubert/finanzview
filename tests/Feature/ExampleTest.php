20106f257254a0bd09cb75a9234c8f15c4fd631b
Alexander <admin@alxserver.de>
2026-09-04 13:35:05 +0200

    Run tests before Docker image push

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,14 +7,12 @@ use Tests\TestCase;
 class ExampleTest extends TestCase
 {
     /**
-     * Die Startseite soll nicht mit einem Serverfehler antworten.
-     *
      * Nicht eingeloggte Benutzer werden zur Login-Seite weitergeleitet.
      */
-    public function test_the_application_redirects_guests_to_login(): void
+    public function test_guests_are_redirected_to_login(): void
     {
         $response = $this->get('/');
 
-        $response->assertRedirect();
+        $response->assertRedirect('/login');
     }
 }
